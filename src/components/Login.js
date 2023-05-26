128043fd09727730ffeff138c4d658c401f08038
kchoi22 <kchoi22@uw.edu>
2023-05-25 22:02:20 -0700

    Adding interaction to Logging process and navbars

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import userData from '../userData.json';
+import { Footer } from './Footer';
+import { Link, useNavigate } from 'react-router-dom';
+import { LoginHeader } from './LoginHeader';
 
-
-
-function LoginPage() {
+function LoginPage({ onLogin }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const navigate = useNavigate();
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -14,13 +16,10 @@ function LoginPage() {
     // Perform login validation
     const user = userData.find((user) => user.email === email);
     if (user) {
-      // Check password or any other authentication logic
       if (password === user.password) {
-        // Successful login
         setError('');
-        console.log('Logged in as:', user.displayName);
-        console.log('Email:', user.email);
-        // Redirect to the desired page or update the login state
+        onLogin(true);
+        navigate('/home');
       } else {
         setError('Incorrect Password');
       }
@@ -29,50 +28,49 @@ function LoginPage() {
     }
   };
 
-  const containerStyle = {
-    backgroundImage: 'url("https://upload.wikimedia.org/wikipedia/commons/5/58/Seattle_Center_as_night_falls.jpg")',
-  };
-
   return (
-    <div className="login-container">
-      <div className="login-box">
-        <h1>Login</h1>
-        <form className="login" onSubmit={handleLogin}>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="Enter Email"
-            aria-label="Input email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <input
-            type="password"
-            id="password"
-            name="password"
-            placeholder="Enter Password"
-            aria-label="Input password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          <button className="login-button" type="submit" role="button">
-            Login
-          </button>
-          {error && <p className="error-message">{error}</p>}
-          <p>
-            Don't have an account? <a href="#">Create an account</a>
-          </p>
-          <p>
-            Forgot your password? <a href="#">Find my account</a>
-          </p>
-        </form>
+    <div className="login-body">
+      <LoginHeader />
+      <div className="login-container">
+        <div className="login-box">
+          <h1>Login</h1>
+          <form className="login" onSubmit={handleLogin}>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter Email"
+              aria-label="Input email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter Password"
+              aria-label="Input password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <button type="submit" className="login-button" role="button">
+              Login
+            </button>
+            {error && <p className="error-message">{error}</p>}
+            <p>
+              Don't have an account? <Link to="/createAccount">Create an account</Link>
+            </p>
+            <p>
+              Forgot your password? <Link to="/forgotPw">Reset Password</Link>
+            </p>
+          </form>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 }
 
-export {LoginPage}
-
+export { LoginPage };
